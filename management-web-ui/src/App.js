739ff965a67e42a6483d09d55ef43fa972151371
Pedro Monteiro <pmapm@ua.pt>
2022-10-28 16:23:50 +0100

    ECP-59 fix build problem

--- management-web-ui/src/App.js
+++ management-web-ui/src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Footer/Footer';
 
 function App() {
   return (
