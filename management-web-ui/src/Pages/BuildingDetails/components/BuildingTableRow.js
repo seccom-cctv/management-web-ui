b61a883a627990cc39d53b268e11718f4ea3b0af
Pedro Monteiro <pmapm@ua.pt>
2022-10-29 15:29:52 +0100

    change edit and delete table icons

--- management-web-ui/src/Pages/BuildingDetails/components/BuildingTableRow.js
+++ management-web-ui/src/Pages/BuildingDetails/components/BuildingTableRow.js
@@ -1,6 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye } from "@fortawesome/free-solid-svg-icons";
 import { AwesomeButton } from 'react-awesome-button';
+import { PencilIcon, TrashIcon } from "@primer/octicons-react"; // custom icons
 import 'react-awesome-button/dist/styles.css';
 import '../BuildingDetails.css';
 
@@ -12,8 +13,8 @@ const BuildingTableRow = (props) => {
             <div className="col col-33" data-label="Health">{props.health}</div>
             <div className="col col-44 col-icon" data-label="Logs"><FontAwesomeIcon icon={faEye} /></div>
             <div className="col col-55 col-icon" data-label="Logs">
-                <AwesomeButton type="primary" onPress={null}>Edit</AwesomeButton>
-                <AwesomeButton style={{marginLeft: '1rem'}} type="danger" onPress={null}>Delete</AwesomeButton>
+                <AwesomeButton type="primary" onPress={null}><PencilIcon /></AwesomeButton>
+                <AwesomeButton style={{marginLeft: '1rem'}} type="danger" onPress={null}><TrashIcon /></AwesomeButton>
             </div>
         </li>
     )
