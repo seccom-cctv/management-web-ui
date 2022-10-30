79da7b28ebb5827c804b295145e50b64b8422107
Pedro Monteiro <pmapm@ua.pt>
2022-10-30 11:04:46 +0000

    ECP-57 create new devices modal

--- management-web-ui/src/Pages/BuildingDetails/BuildingDetails.js
+++ management-web-ui/src/Pages/BuildingDetails/BuildingDetails.js
@@ -1,11 +1,58 @@
 import './BuildingDetails.css';
 import BuildingTableRow from './components/BuildingTableRow';
 import { AwesomeButton } from 'react-awesome-button';
+import Modal from 'react-awesome-modal';
 import 'react-awesome-button/dist/styles.css';
+import { useState } from 'react';
 
 const BuildingDetails = () => {
+    const [visible, setVisible] = useState(false);
+    const [deviceType, setDeviceType] = useState("camera");
+    const [deviceAddress, setDeviceAddress] = useState("");
+
+    const handleDeviceType = (newDevice) => {
+        setDeviceType(newDevice);
+    }
+
+    const handleDeviceAddress = (event) => {
+        var str = event.target.value;
+        setDeviceAddress(str);
+    }
+
+    const OpenModal = () => {
+        setVisible(true);
+    }
+
+    const CloseModal = () => {
+        setVisible(false);
+    }
+
     return (
         <div className='building-details' data-testid="building-details">
+            <Modal visible={visible} width="400" height="350" effect="fadeInDown" onClickAway={CloseModal}>
+                <div className='device-modal'>
+                    <h1 className='device-modal-title'>Add Device</h1>
+                    <div className='device-modal-content'>
+                        <label htmlFor="device-name">Type</label>
+                        <select
+                            id="device-type"
+                            onChange={(event) => handleDeviceType(event.target.value)}
+                            value={deviceType}
+                        >
+                            <option value="camera">Camera</option>
+                            <option value="alarm">Alarm</option>
+                        </select>
+                    </div>
+                    <div className='device-modal-content'>
+                        <label htmlFor="device-address">Address</label>
+                        <input id='device-address' type="text" onChange={handleDeviceAddress} placeholder="Device address..." />
+                    </div>
+                    <div className='device-modal-buttons'>
+                        <AwesomeButton type="primary" onPress={null}>Add</AwesomeButton>
+                        <AwesomeButton type="danger" onPress={CloseModal}>Close</AwesomeButton>
+                    </div>
+                </div>
+            </Modal>
             <h2 className='building-details-header'>Building Details</h2>
             <div className='building-details-content'>
                 <div className='building-details-content-items'>
@@ -18,9 +65,9 @@ const BuildingDetails = () => {
                 </div>
             </div>
             <div className='add-new-camara-button'>
-            <AwesomeButton type="primary" onPress={null}>New Device</AwesomeButton>
+                <AwesomeButton type="primary" onPress={OpenModal}>New Device</AwesomeButton>
             </div>
-            <ul className="responsive-table" style={{paddingLeft: 0}}>
+            <ul className="responsive-table" style={{ paddingLeft: 0 }}>
                 <li className="table-header">
                     <div className="col col-11">Device</div>
                     <div className="col col-22">Date</div>
